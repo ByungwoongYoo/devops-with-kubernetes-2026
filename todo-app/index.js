00468aaf2a97b20ff9e8546ef26e55bcd024a06d
Byungwoong Yoo <yoonge3@gmail.com>
2026-09-16 13:30:14 +0900

    Exercise 1.5: respond to GET /

--- todo-app/index.js
+++ todo-app/index.js
@@ -2,9 +2,15 @@ const http = require('http');
 
 const port = Number(process.env.PORT || 3000);
 
-const server = http.createServer((_req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end('Todo app\n');
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end('<!doctype html><html><body><h1>Todo app</h1><p>DevOps with Kubernetes 2026</p></body></html>');
+    return;
+  }
+
+  res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Not found\n');
 });
 
 server.listen(port, '0.0.0.0', () => {
