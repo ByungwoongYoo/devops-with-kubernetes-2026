23ad6a1d1f02fbe281e603be94961b4d70f5b168
Byungwoong Yoo <yoonge3@gmail.com>
2026-09-16 13:32:43 +0900

    Exercise 1.9: add ping-pong application

---
+++ ping-pong/index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const port = Number(process.env.PORT || 3000);
+let counter = 0;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/pingpong') {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end(`pong ${counter}\n`);
+    counter += 1;
+    return;
+  }
+
+  res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Not found\n');
+});
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Ping-pong server started in port ${port}`);
+});
